15cbc4d0958f84ed5a25198a68ee736be6f6067e
Kevin Pedroza <kevinlarios2343@gmail.com>
2021-02-20 01:08:09 -0600

    Adding the method to return the top and declaring some methods needed

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -3,13 +3,24 @@ class ShortUrlsController < ApplicationController
   # Since we're working on an API, we don't have authenticity tokens
   skip_before_action :verify_authenticity_token
 
-  def index
+  # Returning the 100 top urls
+  def top_url
+    render json: ShortUrl.top_short_url(100), status: 200
   end
 
+  # Return an specific shor url
+  def find_shor_url
+
+  end
+
+  # Redirecting to the specified url
+  def redirect_url
+
+  end
+
+  # Creating the short URL
   def create
-  end
 
-  def show
   end
 
 end
