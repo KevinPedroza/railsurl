bb4ebe82a0dddca6c828eeddb69d145a3317f918
Kevin Pedroza <kevinlarios2343@gmail.com>
2021-02-20 12:38:43 -0600

    Working on saving url on model

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -1,24 +1,26 @@
 class ShortUrl < ApplicationRecord
 
-  CHARACTERS = [*'0'..'9', *'a'..'z', *'A'..'Z'].freeze
+  validate_presence_of: :full_url
 
-  validate_presence_of: :url, :access
+  # The scope to get the top of the short url
+  scope :top_short_url, -> (top_limit) { order('click_count desc').limit(top_limit) }
 
   before_validation :validate_full_url, on: :create
 
-  # The scope to get the top of the short url
-  scope :top_short_url, -> (top_limit) { order('access desc').limit(top_limit) }
-
-  def short_url(url)
-
+  def short_url(full_url)
+    url = ShortUrl.create(full_url: full_url)
+    url.minified_url = "#{ENV.fetch('URL_PROTOCOL')}://#{ENV.fetch('URL_HOSTNAME')}/s/#{MinifyUrlService.instance.url_encode(url.id)}"
+    url.click_count = u.click_count + 1
+    url.save!
+    url
   end
 
   # We validate the URL
   def validate_full_url
-    url = URI.parse(self.url)
+    url = URI.parse(self.full_url)
 
     if(!url.scheme)
-      self.url = "http://#{self.url}"
+      self.url = "http://#{self.full_url}"
     end
   end
 
@@ -27,12 +29,12 @@ class ShortUrl < ApplicationRecord
   end
 
   # We increase the access of the URL
-  def self.increase_access_url(minified_url)
+  def self.increase_click_count_url(minified_url)
     url = where(minified_url: minified_url)
 
     if (url.any?)
       url = url.first
-      url.access += 1
+      url.click_count += 1
       url.save!
       url
     end
