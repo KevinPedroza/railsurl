17acfc41fe7c8dee7ae50956dd54765251d25779
Kevin Pedroza <kevinlarios2343@gmail.com>
2021-02-23 00:18:45 -0600

    Added methods on controller

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -3,21 +3,19 @@ class ShortUrlsController < ApplicationController
   # Since we're working on an API, we don't have authenticity tokens
   skip_before_action :verify_authenticity_token
 
-  # Returning the 100 top urls
-  def top_url
-    render json: ShortUrl.top_short_url(100), status: 200
-  end
-
-  # Return an specific short url
-  def find_short_url
-    url = ShortUrl.find_url(params[:id])
-    render json: {short_url: url}, status: 200
+  def index
+    render json: { urls: ShortUrl.top_short_url(100) }, status: 200
   end
 
   # Redirecting to the specified url
   def redirect_url
-    url = ShortUrl.find_url(params[:id])
-    redirect_to url
+    if (url = ShortUrl.find_url(params[:id]))
+      return redirect_to url
+    end
+
+    render json: {
+      status: 404, errors: 'Not Found'
+    }, status: :not_found
   end
 
   # Creating the short URL
@@ -25,7 +23,7 @@ class ShortUrlsController < ApplicationController
     @url = ShortUrl.short_url(params[:full_url])
 
     if @url.save
-      render json: @url, status: 201
+      render json: { minified_url: @url }, status: 201
     else
       render json: @url.errors, status: :unprocessable_entity
     end
