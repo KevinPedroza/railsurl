e7b635c5ce57dfaac4e4e92b0b2156ae95c13ef6
Kevin Pedroza <kevinlarios2343@gmail.com>
2021-02-23 15:33:08 -0600

    Adding specs all done

--- spec/controllers/short_urls_controller_spec.rb
+++ spec/controllers/short_urls_controller_spec.rb
@@ -1,8 +1,19 @@
 require 'rails_helper'
+ENV["URL_PROTOCOL"] = 'http'
+ENV["URL_HOSTNAME"] = 'localhost:3000'
 
 RSpec.describe ShortUrlsController, type: :controller do
 
   let(:parsed_response) { JSON.parse(response.body) }
+  let(:public_attributes) do
+    {
+      "id"            => short_url.id,
+      "title"         => short_url.title,
+      "full_url"      => short_url.full_url,
+      "minified_url"  => short_url.minified_url,
+      "click_count"   => short_url.click_count,
+    }
+  end
 
   describe "index" do
 
@@ -16,7 +27,7 @@ RSpec.describe ShortUrlsController, type: :controller do
     it "has a list of the top 100 urls" do
       get :index, format: :json
 
-      expect(parsed_response['urls']).to be_include(short_url.public_attributes)
+      expect(parsed_response['urls']).to be_include(public_attributes)
     end
 
   end
@@ -25,32 +36,35 @@ RSpec.describe ShortUrlsController, type: :controller do
 
     it "creates a short_url" do
       post :create, params: { full_url: "https://www.test.rspec" }, format: :json
-      expect(parsed_response['short_code']).to be_a(String)
+      expect(parsed_response['minified_url']).to be_a(Object)
     end
 
     it "does not create a short_url" do
       post :create, params: { full_url: "nope!" }, format: :json
-      expect(parsed_response['errors']).to be_include("Full url is not a valid url")
+      expect(parsed_response['full_url']).to be_include("Full url is not a valid url")
     end
 
   end
 
-  describe "show" do
+  describe "redirect" do
 
     let!(:short_url) { ShortUrl.create(full_url: "https://www.test.rspec") }
 
+
     it "redirects to the full_url" do
-      get :show, params: { id: short_url.short_code }, format: :json
+      short_url.short_url
+      get :redirect_url, params: { id: short_url.minified_url }, format: :json
       expect(response).to redirect_to(short_url.full_url)
     end
 
     it "does not redirect to the full_url" do
-      get :show, params: { id: "nope" }, format: :json
+      get :redirect_url, params: { id: "nope" }, format: :json
       expect(response.status).to eq(404)
     end
 
     it "increments the click_count for the url" do
-      expect { get :show, params: { id: short_url.short_code }, format: :json }.to change { ShortUrl.find(short_url.id).click_count }.by(1)
+      short_url.short_url
+      expect { get :redirect_url, params: { id: short_url.minified_url }, format: :json }.to change { ShortUrl.find(short_url.id).click_count }.by(1)
     end
 
   end
