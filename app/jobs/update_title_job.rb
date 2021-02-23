153ba67e6a3c4385934532b404402bf4a0919933
Kevin Pedroza <kevinlarios2343@gmail.com>
2021-02-23 00:19:28 -0600

    Adding background job

--- app/jobs/update_title_job.rb
+++ app/jobs/update_title_job.rb
@@ -1,7 +1,20 @@
 class UpdateTitleJob < ApplicationJob
+  require 'open-uri'
+
+  include ActiveRecord
+  include HTTParty
+
   queue_as :default
 
   def perform(short_url_id)
+      url = ShortUrl.find(short_url_id)
 
+      response = HTTParty.get(url.full_url)
+      html = Nokogiri::HTML.parse(response.body)
+
+      url.update(title: html.title)
+
+    rescue ActiveRecord::RecordNotFound => e
+      puts e
   end
 end
